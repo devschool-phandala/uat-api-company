8c5e176f4d34d074190751b73ba9141e38af022b
Daky Chanthavong <daki.litd@gmail.com>
2022-02-18 16:28:35 +0700

    Add static verify ssl

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, ".well-known")));
 
 app.use(passport.initialize());
 
